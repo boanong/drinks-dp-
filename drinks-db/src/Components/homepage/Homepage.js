8e9458e130079d4f2b767abf00fccea291c5a78b
Boanong Joshua <boanongjoshua@gmail.com>
2023-03-21 22:36:49 +0100

    Removing link elements linking to unintended pages

--- drinks-db/src/Components/homepage/Homepage.js
+++ drinks-db/src/Components/homepage/Homepage.js
@@ -3,6 +3,8 @@ import './Homepage';
 import './Homepage.css';
 
 const Homepage = () => {
+
+
 const [see, setsee] =useState (true)
   return (
     <div>
@@ -10,10 +12,10 @@ const [see, setsee] =useState (true)
         <a class="active" href="#home">
         Info
         </a>
-        <a href="#hydrangea">Drinks</a>
-        <a href="#lavender">API</a>
+        <a href="#drinks">Drinks</a>
+        <a href="#api">API</a>
         <input type="text" placeholder="Search..." className='find'/>
-        <a href="rose"><i className="fa-solid fa-user" /></a>
+        <a href="icon"><i className="fa-solid fa-user" /></a>
         <div id='menubtn' onClick={() => setsee (!see) }>
           <img src='https://www.lifepng.com/wp-content/uploads/2020/11/Menu-Icon-png-hd.png' alt='menu-button' id='menu' />
       </div>
@@ -33,17 +35,9 @@ const [see, setsee] =useState (true)
       <div className='hero-image'>
 
       <div className='container'>
-        <span className='text first-text'> Relex</span>
-        <span className='text sec-text'> Relex</span>
+        <span className='text first-text'> Relax,</span>
+        <span className='text sec-text'> and have a drink</span>
       </div>
-
-        <h1 className='hero-title'> Relax and have a drink</h1>
-        <div className='searchbtn'>
-          <form>
-            <input type='text' className='type' placeholder='Type...' /> 
-            <input type='submit' className='search' value='Search...' /> 
-          </form>
-        </div>
       </div>
       </div>
     </div>
